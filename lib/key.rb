d1ba4ed3aa60c74e65f61487e8515033d869962c
Sejin Kim <froydroyce@gmail.com>
2019-06-01 12:42:21 -0600

    Create generate_keys method and add keyset attribute

--- lib/key.rb
+++ lib/key.rb
@@ -1,15 +1,18 @@
 class Key
-  attr_reader :numbers
+  attr_reader :numbers, :keyset
 
   def initialize(numbers = random_numbers)
     @numbers = numbers
+    @keyset = generate_keys
   end
 
   def random_numbers
     rand(0..99999).to_s.rjust(5, "0")
   end
 
-  def generate_key
-
+  def generate_keys
+    numbers.chars.each_cons(2).map do |number|
+      number.join.to_i
+    end.map { |joined_num| joined_num.to_i }
   end
 end
