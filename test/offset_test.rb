ea90f2f272c29d039a98ddf61bb1f99c0c392515
Sejin Kim <froydroyce@gmail.com>
2019-06-01 12:59:18 -0600

    Add setup and instance test

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,5 +1,14 @@
 require './test/test_helper.rb'
 require './lib/key'
+require './lib/offset'
 
 class OffsetTest < MiniTest::Test
+  def setup
+    @offset = Offset.new
+  end
+
+  def test_it_exists
+
+    assert_instance_of Offset, @offset
+  end
 end
