402b716be50603d5b71c478be302e3dff25a12a3
Sejin Kim <froydroyce@gmail.com>
2019-06-01 14:14:51 -0600

    Edit instance variable name

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,9 +1,10 @@
 class Offset
-  attr_reader :date, :key
+  attr_reader :date, :key, :offset_keyset
 
   def initialize(key, date = today)
     @key = key
     @date = date
+    @offset_keyset = offset_keys
   end
 
   def today
@@ -14,6 +15,12 @@ class Offset
     date_i = @date.to_i
     (date_i * date_i).to_s.split(//).last(4).map do |number|
       number.to_i
+    end
+  end
+
+  def offset_keys
+    @key.keyset.zip(convert_date).map do |key, offset_key|
+      key + offset_key
     end
   end
 end
