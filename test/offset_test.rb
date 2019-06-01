cb2e49985cce4af3aff5dfa9efea6521055eb552
Sejin Kim <froydroyce@gmail.com>
2019-06-01 13:09:45 -0600

    Add Attributes test for offset

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -4,11 +4,18 @@ require './lib/offset'
 
 class OffsetTest < MiniTest::Test
   def setup
-    @offset = Offset.new
+    @offset = Offset.new("280388")
   end
 
   def test_it_exists
 
     assert_instance_of Offset, @offset
   end
+
+  def test_it_has_attributes
+
+    assert_equal "280388", @offset.date
+  end
+
+
 end
