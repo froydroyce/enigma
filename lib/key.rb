0b048a6197061eec5a1c5c4e096f6d6826cb1732
Sejin Kim <froydroyce@gmail.com>
2019-06-01 11:43:04 -0600

    Add default value to numbers in initialize

--- lib/key.rb
+++ lib/key.rb
@@ -1,11 +1,15 @@
 class Key
   attr_reader :numbers
 
-  def initialize(numbers)
+  def initialize(numbers = random_numbers)
     @numbers = numbers
   end
 
   def random_numbers
     rand(0..99999).to_s.rjust(5, "0")
+  end
+
+  def generate_key
+
   end
 end
