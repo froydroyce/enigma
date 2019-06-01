c26ed7bcdc8d0a72ef44290c3cafd1fec18a9d9e
Sejin Kim <froydroyce@gmail.com>
2019-06-01 13:57:26 -0600

    Create convert_date method

--- lib/offset.rb
+++ lib/offset.rb
@@ -9,4 +9,11 @@ class Offset
   def today
     Date.today.strftime("%d%m%y")
   end
+
+  def convert_date
+    date_i = @date.to_i
+    (date_i * date_i).to_s.split(//).last(4).map do |number|
+      number.to_i
+    end
+  end
 end
