49198b6db58dd65416851fe96f116326cfb68809
Sejin Kim <froydroyce@gmail.com>
2019-06-01 10:43:29 -0600

    Add setup to test

--- test/key_test.rb
+++ test/key_test.rb
@@ -2,9 +2,15 @@ require './test/test_helper.rb'
 require './lib/key'
 
 class KeyTest < MiniTest::Test
+  def setup
+    @key = Key.new
+  end
+
   def test_it_exists
-    key = Key.new
+    @key = Key.new
 
     assert_instance_of Key, key
   end
+
+
 end
