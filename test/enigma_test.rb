fadc381db461cd390fc7c150947b081f9af291ea
Sejin Kim <froydroyce@gmail.com>
2019-06-01 20:32:09 -0600

    Test instance of enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,4 +5,12 @@ require './lib/scrambler'
 require './lib/enigma'
 
 class EnigmaTest < MiniTest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+
+    assert_instance_of Enigma, @enigma
+  end
 end
