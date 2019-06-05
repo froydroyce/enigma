704a959e15046813d929a867c8441049e335413f
Sejin Kim <froydroyce@gmail.com>
2019-06-04 22:36:50 -0600

    Update crack method to be able to use new lines

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,24 +2,25 @@ class Enigma
   def instantiate(msg, key, date, process)
     keyset = Key.new(key).generate_keyset
     offset = Offset.new(date).offset_keys(keyset)
-    { scrambler: Scrambler.new.scramble(offset, msg.downcase, process) }
+    Scrambler.new.scramble(offset, msg.downcase, process)
   end
 
   def encrypt(msg, key = Key.new.numbers, date = Offset.new.date)
     encrypted = instantiate(msg, key, date, "encrypt")
-    { encryption: encrypted[:scrambler], key: key, date: date }
+    { encryption: encrypted, key: key, date: date }
   end
 
   def decrypt(msg, key = Key.new.numbers, date = Offset.new.date)
     decrypted = instantiate(msg, key, date, "decrypt")
-    { decryption: decrypted[:scrambler], key: key, date: date }
+    { decryption: decrypted, key: key, date: date }
   end
 
   def crack(msg, date = Offset.new.date)
-    until msg[-4..-1] == " end"
-      cracked = decrypt(msg, Key.new.numbers, date)
-      msg = cracked[:decryption]
+    until msg[-4..-1] == " end" || msg[-5..-1] == " end\n"
+      key = Key.new.numbers
+      cracked = instantiate(msg, key, date, "decrypt")
+      msg = cracked
     end
-    cracked
+    { decryption: cracked, key: key, date: date }
   end
 end
